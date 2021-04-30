b71abbbfc9cd01abbeb23f804cb0542a659a8f16
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-04-30 15:59:20 +0200

    Risolta questione minuscole/maiuscole

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@
 
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova*/
 // 1. chiedi all'utente il cognome
-var cognomeUtente = prompt("Inserisci qui il tuo cognome").charAt(0).toUpperCase();
+var cognomeUtente = prompt("Inserisci qui il tuo cognome")
 console.log("Il cognome inserito dall'utente è: " + cognomeUtente);
+var cognomeMaiusc= cognomeUtente.substring(0, 1).toUpperCase() + cognomeUtente.substring(1);
+console.log("Il cognome inserito dall'utente (con la prima lettera maiuscola) è: " + cognomeMaiusc);
 
 // 2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
 var cognomeLista = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 
-cognomeLista.push(cognomeUtente)
+cognomeLista.push(cognomeMaiusc)
 
 console.log("Lista aggiornata: " + cognomeLista);
 
