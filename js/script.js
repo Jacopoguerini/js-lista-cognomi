62fb4257d540e12b74696efcefa525f35a0366b5
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-04-30 15:33:15 +0200

    Tentativo di inserire lettera maiuscola

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova*/
 // 1. chiedi all'utente il cognome
-var cognomeUtente = prompt("Inserisci qui il tuo cognome");
-console.log("Il cognome inserito dall'utente è: ");
+var cognomeUtente = prompt("Inserisci qui il tuo cognome").charAt(0).toUpperCase();
+console.log("Il cognome inserito dall'utente è: " + cognomeUtente);
 
 // 2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
 var cognomeLista = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
